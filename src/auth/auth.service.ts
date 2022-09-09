2d710fc8e755676fd50700ce6a88e0f16fa3441d
Roger Schmidt <roger.schp@hotmail.com>
2022-09-09 18:03:26 -0300

    add hash to password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,6 +8,7 @@ import {
   InternalServerErrorException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -19,9 +20,12 @@ export class AuthService {
   async signUp(authCredentialsDto: AuthCredentialsDDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const user = this.usersRepository.create({
       username,
-      password,
+      password: hashedPassword,
     });
 
     try {
