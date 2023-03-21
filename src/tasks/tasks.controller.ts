18881e3184664b9743a53d7af2770ece9039a619
rogerschp <roger.schp@hotmail.com>
2023-03-21 17:35:33 -0300

    restricting deleting a task by user

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -54,7 +54,7 @@ export class TasksController {
   }
 
   @Delete('/:id')
-  deleteTask(@Param('id') id: string): Promise<void> {
-    return this.tasksService.deleteTask(id);
+  deleteTask(@Param('id') id: string, @GetUser() user: User): Promise<void> {
+    return this.tasksService.deleteTask(id, user);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -39,7 +39,7 @@ export class TasksService {
 
   async getTaskById(id: string, user: User): Promise<Task> {
     const found = await this.tasksRepository.findOne({
-      where: { id: id, user: user },
+      where: { id, user },
     });
 
     if (!found) {
@@ -72,8 +72,8 @@ export class TasksService {
     return task;
   }
 
-  async deleteTask(id: string): Promise<void> {
-    const result = await this.tasksRepository.delete({ id: id });
+  async deleteTask(id: string, user: User): Promise<void> {
+    const result = await this.tasksRepository.delete({ id, user });
     if (result.affected === 0) {
       throw new NotFoundException(`Task with ID "${id} not found!`);
     }
